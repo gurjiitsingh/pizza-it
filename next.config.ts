e19c68253381b00fb256135d7a7299ae400fd080
gurnit <gurjiitsingh@gmail.com>
2025-12-15 20:00:38 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-
-  // Next.js 16 no longer supports "eslint" option in next.config.
-  // Removed to avoid type error.
-
   env: {
     PAYPAL_CLIENT_ID:
       "BAAQambvfgf8cMiIWoROWluTo5X08lvESisQno-RXyWIK7Mk8JzNL7UNonzp8h5g5ZGjd8HTp2vBHD_4zk",
